cfff70b0665d98c2be1bcbf7e05c725190704b2a
michel_929 <michel.gruenig@gmail.com>
2023-01-26 07:17:27 +0100

    Cookies

--- templates/head.php
+++ templates/head.php
@@ -11,7 +11,8 @@
 <head>
 
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-
+    <meta name="audience" content="all" />
+    <meta name="keywords" content="Sensivity, Gaming, ESport, Team Sensivity" />
     <meta content="TeamSensivity | Eine Community und alle Games an einem Ort" name="description">
     <meta content="michel929" name="author">
 
@@ -38,6 +39,7 @@ echo "Webseite";
     <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
     <!-- !Google Fonts -->
 
     <!-- Styles -->
