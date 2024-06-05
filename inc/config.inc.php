7ba38ad6de78e6710d1d64a15d36709a7edae741
michel_929 <michel.gruenig@gmail.com>
2024-06-05 08:45:42 +0200

    Update config.inc.php
    Passwort war sichtbar wurde nun geändert und neue User wurden eingeführt.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -2,5 +2,5 @@
 $db_host = 'sensivity:3306';
 $db_name = 'TeamSensivity';
 $db_user = 'discord';
-$db_password = 'HGpcejWBMZF4V3kM2h7y';
+$db_password = '';
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
